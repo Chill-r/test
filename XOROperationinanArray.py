512bb1e48d0d14289a4f7c51773bb72d7ea20746
xiaoyaxiaohanxin <50623110+xiaoyaxiaohanxin@users.noreply.github.com>
2020-06-26 00:30:39 +0800

    Create XOROperationinanArray.py

---
+++ XOROperationinanArray.py
@@ -0,0 +1,16 @@
+#1486. XOR Operation in an Array
+
+'''
+Given an integer n and an integer start.
+Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+Return the bitwise XOR of all elements of nums.
+'''
+#easy
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        result = 0
+        for i in range(n):
+            result ^= start+2*i
+        return result
+
