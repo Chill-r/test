aca5eff3955d7016f51323618ab6e1975241cb51
xiaoyaxiaohanxin <50623110+xiaoyaxiaohanxin@users.noreply.github.com>
2020-06-30 11:34:39 +0800

    Create LCP06.py

---
+++ LCP06.py
@@ -0,0 +1,15 @@
+#LCP 06. 拿硬币
+'''
+桌上有 n 堆力扣币，每堆的数量保存在数组 coins 中。我们每次可以选择任意一堆，拿走其中的一枚或者两枚，求拿完所有力扣币的最少次数。
+示例 1：
+输入：[4,2,1]
+输出：4
+解释：第一堆力扣币最少需要拿 2 次，第二堆最少需要拿 1 次，第三堆最少需要拿 1 次，总共 4 次即可拿完。
+'''
+#easy
+class Solution:
+    def minCount(self, coins: List[int]) -> int:
+        result=0
+        for i in range(len(coins)):
+            result+=coins[i]//2+coins[i]%2
+        return result
